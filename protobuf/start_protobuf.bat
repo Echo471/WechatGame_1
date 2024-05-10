314c18f86d0c376effe7ec809d6755c6d1495c7f
littleGirlWjs <17547625284@163.com>
2024-05-10 23:06:17 +0800

    修改生成proto文件

--- protobuf/start_protobuf.bat
+++ protobuf/start_protobuf.bat
@@ -1,17 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
-set "dirPath=.\..\server\"
+@REM set "dirPath=.\..\server\"
 
-@echo start delete server old .proto files
-for /f "delims=" %%i in ('dir "%dirPath%" /b /s /a-d-h') do (
-    echo Deleting: %%i
-    if exist "%%i" (
-        del /q "%%i"
-    )
-    if exist "%%i" (
-        rmdir /s /q "%%i"
-    )
-)
+@REM @echo start delete server old .proto files
+@REM for /f "delims=" %%i in ('dir "%dirPath%" /b /s /a-d-h') do (
+@REM     echo Deleting: %%i
+@REM     if exist "%%i" (
+@REM         del /q "%%i"
+@REM     )
+@REM     if exist "%%i" (
+@REM         rmdir /s /q "%%i"
+@REM     )
+@REM )
 
 @echo .
 @echo .
@@ -46,7 +46,7 @@ protoc.exe  .\proto\*.proto --plugin=protoc-gen-ts.cmd --ts_out=..\client\assets
 @echo .
 @echo .
 
-generate_mapping.exe --protoc_path="protoc.exe" --protoc_ts_plugin_path="protoc-gen-ts.cmd" --proto_files_path=proto\*.proto --message_ids_file_ts=..\client\assets\scripts\net\protobuf\message_ids.ts --message_ids_file_java=..\server\message_ids.java
+:: generate_mapping.exe --protoc_path="protoc.exe" --protoc_ts_plugin_path="protoc-gen-ts.cmd" --proto_files_path=proto\*.proto --message_ids_file_ts=..\client\assets\scripts\net\protobuf\message_ids.ts --message_ids_file_java=..\server\message_ids.java
 
 
 @echo .
