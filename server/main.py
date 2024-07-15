a9fb45b65f31eebb46947372359d9ab51f61a402
wangjinsheng <17547625284@163.com>
2024-07-15 23:19:09 +0800

    服务端基础框架

--- server/main.py
+++ server/main.py
@@ -1,7 +1,14 @@
+# -*- coding: utf-8 -*-
 from const.module_const import EModule
 from module.log_module import LogModule
 from module.module_register import ModuleRegister
 
-if __name__ == '__main__':
+
+def register_module():
     # register module
+    ## Log 是最基础的模块 需要最先注册
     ModuleRegister.register(EModule.LOGGER, LogModule())
+
+
+if __name__ == '__main__':
+    register_module()
